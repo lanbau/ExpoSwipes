6283db30b400813c119819d5da6e153528431e08
Irvin Lin <irvinln@gmail.com>
2019-04-18 15:39:35 +0800

    working version

--- App.js
+++ App.js
@@ -20,6 +20,11 @@ export default class App extends React.Component {
     }
     console.log(-ScreenWidth/2)
     //https://facebook.github.io/react-native/docs/animations#interpolation
+
+    // IF X increases, rotate towards 10 degrees
+    // IF X decreases, rotate towards -10 degrees
+
+
     this.rotate = this.position.x.interpolate({
       inputRange: [ 
         -ScreenWidth/2, 
@@ -44,6 +49,67 @@ export default class App extends React.Component {
       ]
     }
 
+    // If screenwidth is 200 for example, 
+    // if x position is 200 or 150 within the range then opacity is 1 (Show like button)
+
+    this.likeOpacity = this.position.x.interpolate({
+      inputRange: [ 
+        -ScreenWidth/2, 
+        0, 
+        ScreenWidth/2
+      ],
+      outputRange: [
+        0, 
+        0, 
+        1
+      ],
+      extrapolate: 'clamp'
+    })
+
+    // If screenwidth is -299 for example, 
+    // if x position is -299 or -250 within the range then opacity is 1 (Show dislike button)
+    this.dislikeOpacity = this.position.x.interpolate({
+      inputRange: [ 
+        -ScreenWidth/2, 
+        0, 
+        ScreenWidth/2
+      ],
+      outputRange: [
+        1, 
+        0, 
+        0
+      ],
+      extrapolate: 'clamp'
+    })
+
+    this.nextCardOpacity = this.position.x.interpolate({
+      inputRange: [ 
+        -ScreenWidth/2, 
+        0, 
+        ScreenWidth/2
+      ],
+      outputRange: [
+        1, 
+        0, 
+        1
+      ],
+      extrapolate: 'clamp'
+    })
+
+    this.nextCardScale = this.position.x.interpolate({
+      inputRange: [ 
+        -ScreenWidth/2, 
+        0, 
+        ScreenWidth/2
+      ],
+      outputRange: [
+        1, 
+        0.8, 
+        1
+      ],
+      extrapolate: 'clamp'
+    })
+
   }
 
   componentWillMount () {
@@ -55,7 +121,32 @@ export default class App extends React.Component {
         this.position.setValue({x: gestureState.dx, y: gestureState.dy})
       },
       onPanResponderRelease: (evt, gestureState) => {
-
+        if (gestureState.dx > 120) {
+          Animated.spring(this.position, {
+            toValue: { x: ScreenWidth + 100, y: gestureState.dy }
+          })
+          .start(() => {
+            this.setState({currentIndex: this.state.currentIndex + 1}, () => {
+              this.position.setValue({ x: 0, y: 0})
+            })
+          })
+        }
+        else if (gestureState.dx < -120) {
+          Animated.spring(this.position, {
+            toValue: { x: -ScreenWidth - 100, y: gestureState.dy }
+          })
+          .start(() => {
+            this.setState({currentIndex: this.state.currentIndex + 1}, () => {
+              this.position.setValue({ x: 0, y: 0})
+            })
+          })
+        } else {
+          // Put back image to original
+          Animated.spring(this.position, {
+            toValue: { x:0, y:0 },
+            friction: 4
+          }).start()
+        }
       }
     })
   }
@@ -84,6 +175,28 @@ export default class App extends React.Component {
               }
             ]}
           >
+            <Animated.View 
+              style={{
+                opacity: this.likeOpacity, 
+                transform: [{rotate: '-30deg'}], 
+                position: 'absolute', 
+                top:50, 
+                left:40, 
+                zIndex:1000
+              }}>
+              <Text style={{color:'white', fontSize: 40}}>LIKE</Text>
+            </Animated.View>
+            <Animated.View 
+              style={{ 
+                opacity: this.dislikeOpacity, 
+                transform: [{rotate: '30deg'}], 
+                position: 'absolute', 
+                top:50, 
+                right:40, 
+                zIndex:1000
+              }}>
+              <Text style={{color:'red', fontSize: 40}}>NOPE</Text>
+            </Animated.View>
             <Image
               style={{ flex: 1, borderRadius: 20 }}
               source={{ uri: item.uri }}
@@ -100,6 +213,8 @@ export default class App extends React.Component {
             style={
             [
               { 
+                opacity: this.nextCardOpacity, 
+                transform: [{scale: this.nextCardScale}], 
                 height: ScreenHeight - 120, 
                 width: ScreenWidth,
                 padding: 10,
